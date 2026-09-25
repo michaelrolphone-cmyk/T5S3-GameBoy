4eb08a46be5bc50bc9071fd4a84f1622b488bff2
michaelrolphone-cmyk <michael.rol.phone@gmail.com>
2026-09-25 12:21:39 -0600

    Test 12-hour clock formatting

--- tests/game_clock_test.cpp
+++ tests/game_clock_test.cpp
@@ -10,12 +10,15 @@
 
 int main() {
   paperboy_game_clock_sync(12U * 3600U + 34U * 60U + 56U, 1000000LL);
-  assert(strcmp(paperboy_game_clock_label(1000000LL), "12:34") == 0);
-  assert(strcmp(paperboy_game_clock_label(4999999LL), "12:34") == 0);
-  assert(strcmp(paperboy_game_clock_label(5000000LL), "12:35") == 0);
+  assert(strcmp(paperboy_game_clock_label(1000000LL), "12:34 PM") == 0);
+  assert(strcmp(paperboy_game_clock_label(4999999LL), "12:34 PM") == 0);
+  assert(strcmp(paperboy_game_clock_label(5000000LL), "12:35 PM") == 0);
 
   paperboy_game_clock_sync(23U * 3600U + 59U * 60U + 50U, 0);
-  assert(strcmp(paperboy_game_clock_label(15000000LL), "00:00") == 0);
+  assert(strcmp(paperboy_game_clock_label(15000000LL), "12:00 AM") == 0);
+
+  paperboy_game_clock_sync(8U * 3600U + 7U * 60U, 0);
+  assert(strcmp(paperboy_game_clock_label(0), "08:07 AM") == 0);
 
   paperboy_game_clock_sync(0U, 0);
   assert(strcmp(paperboy_game_clock_label(60000000LL), "--:--") == 0);
